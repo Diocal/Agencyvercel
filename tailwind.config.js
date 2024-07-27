cfe15e47ee2b2dc8d1724785d940259be94edebf
diego calderon <diegocalderon9999@gmail.com>
2024-07-27 07:24:31 +0300

    update 27/07

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { default: daisyui } = require("daisyui");
+
 module.exports = {
   content: [
     './src/**/*.{astro,js,ts,jsx,tsx,vue,svelte}',
@@ -6,5 +8,13 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  variants: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui:{
+    themes:[
+      'light', 'dark', 'cupcake', 'bumblebee', 'emerald', 'corporate', 'synthwave', 'retro', 'cyberpunk', 'valentine', 'halloween', 'garden', 'forest', 'aqua', 'lofi', 'pastel', 'fantasy', 'wireframe', 'black', 'luxury', 'dracula'
+    ],
+  }
 };
